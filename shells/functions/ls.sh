b2a7844f92f661c9b610a3c3b57e4363ba27b304
veggie <me>
2013-04-15 17:54:54 -0400

    better gnu/mac and ls/gls handling

--- shells/functions/ls.sh
+++ shells/functions/ls.sh
@@ -1,47 +1,82 @@
-# vim:ts=4
+# ls alias setup
+#
+# Test for gnu ls, mac ls, set colors, set aliases.  
+#
+# These aliases are set: plain "ls" with color options   and   l   l.  ll   d
 
-# ls alias setup. 
-# since we have to test for gnu ls and a few other
-# things, we put all of this in a separate file.
+veggieboot_lsfile=$TMPDIR/veggieboot_ls_$$
 
-# test for GNU ls on the path, and set dir colors
-ls --version random_string_4231784713289fdjsklfjlsaFDSAFASD > /tmp/tmp_ls_$$ 2>&1
-ls_ver_tmp=`cat /tmp/tmp_ls_$$ | grep "Free Software Foundation"`
-if [ ! -z "$ls_ver_tmp" ]; then
-	# we are using GNU ls
+if [ "$VEGGIE_ARCH" = "osx" ]; then
+	# We test OSX first, since we may wish to override with GNU ls settings.
 
-	if [ -e "$VEGGIE_HOME/utils/dircolors" ]; then 
-		# this sets $LS_COLORS for colors
-		eval `dircolors --bourne-shell "$VEGGIE_HOME/utils/dircolors"`
-	fi
-	alias ls='ls --color=auto'
-	# h is GNU & BSD for 'human numbers in -l', so, 1.6M not 1648576
-	alias d='ls -Flho'
-
-elif [ "$VEGGIE_ARCH" = "osx" ]; then
 	# For OSX, -G does color like GNU ls, but without many
 	# enhancements.  To set the colors, see 'man ls' on
 	# plain-raw OS X and set the environment var here.
 	# The chosen colors could use refinement, but they are close
-	# to matching the dircolors above.
-
+	# to matching the dircolors configuation above.
 	export LSCOLORS=GxgxfxDxcxDxDxDbDbDbDb
-
 	alias ls='ls -G'
-	# Some point between 10.4 & 10.7, -o became supported!
-	alias d='ls -Flho'
 
-else 
+	# Some point between 10.4 & 10.7, -o became supported for ls -l!
+	alias d='ls -Flho'
+	alias ll='ls -AhsF'
+	alias l='ls -hsF'
+
+	# mac ls is still useful for extended attributes, so alias the old version
+	alias mls='/bin/ls'
+	alias macls='/bin/ls'
+fi
+
+
+# test for GNU ls on the path, and set dir colors
+ls --version random_string_4231784713289fdjsklfjlsaFDSAFASD > $veggieboot_lsfile.ls 2>&1
+gls --version random_string_4231784713289fdjsklfjlsaFDSAFASD > $veggieboot_lsfile.gls 2>&1
+
+veggieboot_ls=`cat $veggieboot_lsfile.ls | grep "Free Software Foundation"`
+veggieboot_gls=`cat $veggieboot_lsfile.gls | grep "Free Software Foundation"`
+
+if [ ! -z "$veggieboot_ls" -o ! -z "$veggieboot_gls" ]; then
+	# we are using GNU ls
+
+	veggieboot_dc=`type -P dircolors`
+	veggieboot_gdc=`type -P gdircolors`
+
+	if [ ! -z "$veggieboot_dc" -o ! -z "$veggieboot_gdc" ]; then
+		# this sets $LS_COLORS for colors from dircolors or gdircolors
+		if [ -e "$VEGGIE_HOME/utils/dircolors" ]; then 
+			eval `${veggieboot_dc:-$veggieboot_gdc} --bourne-shell "$VEGGIE_HOME/utils/dircolors"`
+		else
+			eval `${veggieboot_dc:-$veggieboot_gdc} --bourne-shell `
+		fi
+	fi
+
+	# are we using gls or just ls
+	if [ ! -z "$veggieboot_ls"  ]; then
+		alias ls='ls --color=auto'
+	else
+		alias ls='gls --color=auto'
+	fi
+
+	# h is GNU & BSD for 'human numbers in -l', so, 1.6M not 1648576
+	alias d='ls -Flho'
+	alias ll='ls -AhsF'
+	alias l='ls -hsF'
+fi
+
+if [ -z "$veggieboot_ls" -a -z "$veggieboot_gls" -a  "$VEGGIE_ARCH" = "osx" ]; then
+	# we're not using osx and don't have gnu ls, so the defaults
+	# -h not supported on sys V
 	alias d='ls -Flo'
+	alias ll='ls -AsF'
+	alias l='ls -sF'
 fi
 
 # special glob in this alias only works in bash
 if [ ! -z "$BASH_VERSION" ]; then
-	alias l.='ls -Fd .[a-zA-Z0-9]* '
+	alias l.='ll -FAd . .?* '
 fi
 
-alias ll='ls -AsF'
-alias l='ls -sF'
+rm $veggieboot_lsfile.ls $veggieboot_lsfile.gls
+unset veggieboot_ls veggieboot_gls veggieboot_lsfile veggieboot_dc veggieboot_gdc
 
-rm /tmp/tmp_ls_$$
-unset ls_ver_tmp
+# vim:ts=4

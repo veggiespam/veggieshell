e5b25d04a4a9273153b9fc36f46f9e1dda553e41
veggie <no_one>
2013-10-26 12:40:38 -0400

    added a few more options.

--- run-once/osx/general-defaults.sh
+++ run-once/osx/general-defaults.sh
@@ -9,12 +9,33 @@
 defaults write com.apple.mail DisableSendAnimations -bool YES
 defaults write com.apple.mail DisableReplyAnimations -bool YES
 
-# Disable reflection/glass Dock appearance (10.5+)
+# Disable 3D reflection/glass Dock appearance & make 2D (10.5+)
 defaults write com.apple.dock no-glass -boolean YES
 
 # Make hidden apps dimmed in Dock (10.3+)
 defaults write com.apple.dock showhidden -bool true
 
+# Turn on App Store debug menu, very useful (10.7+)
+#    https://discussions.apple.com/message/22992220#22992220
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+# Enable debug menu in iCal (10.5-10.8)
+#    http://apple.stackexchange.com/questions/58889/how-do-i-enable-the-debug-menu-in-calendar
+defaults write com.apple.iCal IncludeDebugMenu 1
+
+# Enable debug menu in Safari (10.4+)
+#    http://osxdaily.com/2011/11/07/safari-debug-menu/
+defaults write com.apple.Safari IncludeInternalDebugMenu 1
+
+# Disable the window shadow in screen shots; helpful when sending reports.
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# these appear to be ignored in 10.9
+#    http://apple.stackexchange.com/questions/61924/how-can-i-remove-the-window-shadow-in-mountain-lion
+# but this works wonderfully -- compiled app
+#    https://github.com/puffnfresh/toggle-osx-shadows
+defaults write NSGlobalDomain NSUseLeopardWindowValues NO
+defaults write com.apple.Safari NSUseLeopardWindowValues YES
 
 
 killall Dock

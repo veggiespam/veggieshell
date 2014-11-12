079bd2738070fd2cfa1e7ad7f532d114df3679e6
veggie <me>
2014-11-12 15:52:29 -0500

    fix *.md & add en0 for new macs

--- scripts/change-mac-addr.sh
+++ scripts/change-mac-addr.sh
@@ -6,6 +6,9 @@ if [ $VEGGIE_ARCH != osx ]; then
     return
 fi
 
+eth_interface=en0
+
+# Ideas came from:
 # http://osxdaily.com/2008/01/17/how-to-spoof-your-mac-address-in-mac-os-x/
 # http://superuser.com/questions/218340/how-to-generate-a-valid-random-mac-address-with-bash-shell
 
@@ -20,21 +23,21 @@ prefix="D4:BE:A9"
 suffix=`hexdump -n3 -e'/1 ":%02X"' /dev/random`
 new_addr="$prefix$suffix"
 
-echo Will try to change to this: $new_addr
-echo Old Address:
-ifconfig en1 | grep ether
+echo    Will try to change to this: $new_addr
+echo -n Here is your current MAC  :
+ifconfig $eth_interface | grep ether
 
-echo Your current network is:
+echo -n Your current network is   :
 $airport_cmd -I | grep " SSID:"
 
 echo Shutting down wi-fi:
 $airport_cmd -z
 
 echo Setting new MAC to $new_addr -- you must type root password
-sudo ifconfig en1 ether $new_addr
+sudo ifconfig $eth_interface ether $new_addr
 
 echo Done setting, running a verification check:
-ifconfig en1 | grep ether
+ifconfig $eth_interface | grep ether
 
 echo Done, now try associating to your new network.
 

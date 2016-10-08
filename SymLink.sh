a6510a453779e23f1881848192cd87e71c777409
veggie spam <no_email>
2016-10-08 15:10:21 -0400

    bettter symlinks

--- SymLink.sh
+++ SymLink.sh
@@ -2,7 +2,11 @@
 
 tmpfile="$HOME/veggieshell_installer_tmpfile_$$.sh"
 cd "$HOME"
-VEGGIE_HOME="$HOME/.veggieshell"
+export VEGGIE_HOME="$HOME/.veggieshell"
+
+# we want relative links in the home dir, so this is the linkage name (as opposed to the full path above)
+# This export is only for this script and isn't used for real.
+export dotVS=".veggieshell"
 
 if [ ! -f "$VEGGIE_HOME/FileList" ]; then
 	echo You must run SymLink.sh from your home dir.
@@ -16,12 +20,14 @@ mkdir -p Before-Veggieshell
 
 cat "$VEGGIE_HOME/FileList" | \
 	grep -v '^#' | \
-	awk '{printf "if [ -e \"%s\" ] ; then mv -f \"%s\" Before-Veggieshell \nfi \n ln -s \"$VEGGIE_HOME/%s\" \"%s\" ;  \n", $1, $1, $2, $1}' \
+	awk '{printf "if [ -e \"%s\" ] ; then mv -f \"%s\" Before-Veggieshell \nfi \n ln -s \"$dotVS/%s\" \"%s\" ;  \n", $1, $1, $2, $1}' \
 	> "$tmpfile"
 
 . "$tmpfile"
 
 rm -f "$tmpfile"
 unset tmpfile
+
+echo Done, log out and back in.  See the bugs and todo list.
 
 # vi:ts=4:sts=4

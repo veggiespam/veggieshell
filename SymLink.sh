7f547cea9fb8be0c0b2c15eb5dc59727ba22d080
veggie spam <no_email>
2017-04-18 16:00:06 -0400

    broken symlinks in home dir cause issues when run

--- SymLink.sh
+++ SymLink.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 tmpfile="$HOME/veggieshell_installer_tmpfile_$$.sh"
-cd "$HOME"
-export VEGGIE_HOME="$HOME/.veggieshell"
+cd ${HOME}
+export VEGGIE_HOME=${HOME}/.veggieshell
 
 # we want relative links in the home dir, so this is the linkage name (as opposed to the full path above)
 # This export is only for this script and isn't used for real.
@@ -18,14 +18,16 @@ echo Before-Veggieshell directory.
 
 mkdir -p Before-Veggieshell
 
+# Note for test: -e is exists and -L is needed in cases of broken symlinks
+#
 cat "$VEGGIE_HOME/FileList" | \
 	grep -v '^#' | \
-	awk '{printf "if [ -e \"%s\" ] ; then mv -f \"%s\" Before-Veggieshell \nfi \n ln -s \"$dotVS/%s\" \"%s\" ;  \n", $1, $1, $2, $1}' \
-	> "$tmpfile"
+	awk '{printf "if [ -e \"%s\" -o -L \"%s\" ] ; then\n   mv -f \"%s\" Before-Veggieshell \nfi \n ln -s \"$VEGGIE_HOME/%s\" \"%s\" ;  \n", $1, $1, $1, $2, $1}' \
+	> ${tmpfile}"
 
-. "$tmpfile"
+. ${HOME}/${tmpfile}
 
-rm -f "$tmpfile"
+rm -f ${tmpfile}
 unset tmpfile
 
 echo Done, log out and back in.  See the bugs and todo list.
